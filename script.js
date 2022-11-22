7869668f0fdfbdcd499d9003f414141ca0d2bef8
Sachin Sharma <sachin.kumarsharam09@gmail.com>
2022-11-22 22:00:37 +0530

    complete date and time display section

--- script.js
+++ script.js
@@ -1,30 +1,89 @@
-const cityName = document.querySelector(".city-name");
-const API_key = "8d86d7c63d6b5ff4900438e1d3c71f85";
-//const city = cityName.value;
-const city = `Mumbai`;
-const url = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${API_key}`;
-//const url2 = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}`;
+let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+];
 
-// fetch(url)
-//     .then((response) => {
-//         //console.log(response.json());
-//         return response.json();
-//     })
-//     .then((data) => {
-//         // console.log(data);
-//         // let { lat, lon } = data[0];
-//         // console.log(lat);
-//         // console.log(lon);
-//         return data;
-//     })
-//     .then((data) => {
-//         fetch(
-//             `https://api.openweathermap.org/data/2.5/weather?lat=${data[0].lat}&lon=${data[0].lon}&appid=${API_key}`
-//         )
-//             .then((newResponse) => {
-//                 return newResponse.json();
-//             })
-//             .then((newData) => {
-//                 console.log(newData.main.humidity);
-//             });
-//     });
+const hourVal = document.querySelector(".hour-value");
+const minVal = document.querySelector(".min-value");
+const secVal = document.querySelector(".sec-value");
+
+const date = document.querySelector(".date");
+const month = document.querySelector(".month");
+const year = document.querySelector(".year");
+
+getDateValues();
+setTimeValues(getTimeValues().hour, getTimeValues().min, getTimeValues().sec);
+setDateValues(
+    getDateValues().dateVal,
+    months[getDateValues().monthVal],
+    getDateValues().yearVal
+);
+
+setInterval(function () {
+    setDateValues(
+        getDateValues().dateVal,
+        months[getDateValues().monthVal],
+        getDateValues().yearVal
+    );
+    setTimeValues(
+        getTimeValues().hour,
+        getTimeValues().min,
+        getTimeValues().sec
+    );
+}, 1000);
+
+//functions statements/ Declarations
+
+function getTimeValues() {
+    const currDate = new Date();
+    const hour =
+        currDate.getHours() < 10
+            ? `0${currDate.getHours()}`
+            : currDate.getHours();
+    const min =
+        currDate.getMinutes() < 10
+            ? `0${currDate.getMinutes()}`
+            : currDate.getMinutes();
+    const sec =
+        currDate.getSeconds() < 10
+            ? `0${currDate.getSeconds()}`
+            : currDate.getSeconds();
+
+    return {
+        hour: hour,
+        min: min,
+        sec: sec,
+    };
+}
+
+function getDateValues() {
+    const currDate = new Date();
+
+    return {
+        dateVal: currDate.getDate(),
+        monthVal: currDate.getMonth(),
+        yearVal: currDate.getFullYear(),
+    };
+}
+
+function setTimeValues(hour, min, seconds) {
+    hourVal.textContent = hour;
+    minVal.textContent = min;
+    secVal.textContent = seconds;
+}
+
+function setDateValues(dateVal, monthVal, yearVal) {
+    date.textContent = dateVal;
+    month.textContent = monthVal;
+    year.textContent = yearVal;
+}
